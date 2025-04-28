7464845ffd0f6b2efe3bcd90d308fb87311ebd2c
cmatteo <camilo.matteo@babelgroup.com>
2025-04-28 14:53:35 -0400

    :package: chakraUI added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ButtonExample } from './components/ButtonExample';
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <ChakraProvider>
+      <div className="content">
+        <h1>Rsbuild with React</h1>
+        <p>Start building amazing things with Rsbuild.</p>
+        <ButtonExample />
+      </div>
+    </ChakraProvider>
   );
 };
 
---
+++ src/components/ButtonExample.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+export const ButtonExample = () => {
+  return (
+    <Button colorScheme="teal" variant="outline">
+      Button
+    </Button>
+  );
+};
